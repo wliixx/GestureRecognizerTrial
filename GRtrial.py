f4233a47cdd56f61207e86370b8f23f813d3129e
higuko <novavita482@gmail.com>
2026-09-06 16:11:32 +0300

    fadfFWFEwfeWFEW

--- GRtrial.py
+++ GRtrial.py
@@ -1,5 +1,6 @@
+import cv2
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+from mediapipe.tasks import python as mp_tasks
+from mediapipe.tasks.python import vision as mp_vision
 
 model_path = ''
